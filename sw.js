e459e911dc6c6cd950d73d707f803a00fa705d3e
Aunt-Tia <social@skytia.com>
2026-07-25 21:56:24 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
-const CACHE_NAME = 'skytia-v1';
+const CACHE_NAME = 'skytia-v2';
 const urlsToCache = ['/'];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.map(name => {
+        if (name !== CACHE_NAME) return caches.delete(name);
+      }))
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
